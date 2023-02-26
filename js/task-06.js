7a3f882c4daca75cf1485a87f9ec6e6ed16e11b3
RuslanaSemenenko <113388430+RuslanaSemenenko@users.noreply.github.com>
2023-02-26 11:39:34 +0000

    js

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,7 @@
 const validationInput = document.querySelector('#validation-input');
 
-validationInput.addEventListener('blur', event => {
-
-    if (event.target.value.length == validationInput.getAttribute('data-length')) {
-
-        validationInput.classList.add('valid')
-
-        if (validationInput.classList.contains('invalid')) {
-            validationInput.classList.remove('invalid')
-        }
-    } else {
-        if (validationInput.classList.contains('valid')) {
-          validationInput.classList.remove('valid')
-        }
-
-        validationInput.classList.add('invalid')
-    }
+validationInput.addEventListener("blur", () => {
+    validationInput.classList.toggle("valid", validationInput.value.length >= 6);
+    
+    validationInput.classList.toggle("invalid", validationInput.value.length < 6);
 });
--- js/task-08.js
+++ js/task-08.js
@@ -9,7 +9,7 @@ function handleSubmit(event) {
         elements: { email, password },
     } = event.currentTarget;
 
-    if (email.value === "" || password.value === "") {
+    if (email.value.trim() === "" || password.value === "") {
         return alert("Заповни всі поля!");
     }
 
